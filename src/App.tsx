6ca0e9545f852761a4db632ff58f0b1834851b96
Daniel Fadamitan <b.fadamitan2019@gmail.com>
2024-01-22 13:02:22 +0100

    issued

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import Auth from "./pages/auth";
 
-import { Courses, Testimonial } from "./components/landing";
-
 export default function App() {
   return (
     <section>
       <Auth />
-      <Courses />
-      <Testimonial />
     </section>
   );
 }
