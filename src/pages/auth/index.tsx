10628d5379c7a9fc7a7f2f48eaf42ac6f43d2a2c
deralph <jraphael441@gmail.com>
2024-01-25 16:15:13 +0100

    corrected auth image styling

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,7 +2,7 @@ import { IoMdArrowBack } from "react-icons/io";
 
 import ExternalAuth from "./ExternalAuth";
 import SignUp from "./SignUp";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import LogIn from "./LogIn";
 import ResetPwd from "./ResetPwd";
 import { useNavigate } from "react-router";
@@ -20,7 +20,16 @@ export default function Auth() {
     SignUp: false,
     recover: false,
   });
+  const [height, setHeight] = useState<number>(0);
 
+  const myFormRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (myFormRef.current) {
+      // Access the offsetHeight property to get the height
+      setHeight(myFormRef.current.offsetHeight);
+      // console.log("Element height:", height);
+    }
+  }, [controlAuth]);
   const handleAuthControl = (actionType: string) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     setControlAuth((prevControlAuth: control | any) => {
@@ -57,7 +66,10 @@ export default function Auth() {
         }}
       />
       <div className="flex items-center bg-auth bg-no-repeat   bg-cover bg-center  p-[5%] lg:p-0 lg:[background-image:none] ">
-        <div className="lg:basis-1/2 flex-1 w-full h-full bg-[rgba(225,225,225,0.9)] lg:bg-white p-[5%]">
+        <div
+          ref={myFormRef}
+          className="lg:basis-1/2 flex-1 w-full h-full bg-[rgba(225,225,225,0.9)] lg:bg-white p-[5%]"
+        >
           {!controlAuth.recover && (
             <div className=" flex justify-evenly mb-6">
               <p
@@ -96,7 +108,10 @@ export default function Auth() {
             {!controlAuth.recover && <ExternalAuth />}
           </form>
         </div>
-        <div className="lg:basis-1/2 flex-1 h-full min-h-[700px] hidden lg:block bg-auth bg-no-repeat bg-cover bg-center"></div>
+        <div
+          style={{ height }}
+          className="lg:basis-1/2 flex-1   min-h-[700px] hidden lg:block bg-auth bg-no-repeat bg-contain bg-center "
+        ></div>
       </div>
       <div className="mt-[10%]" />
       <Footer />
