e52c2f118c116d29fe1d4d091c1a237abc11e306
deralph <jraphael441@gmail.com>
2024-01-25 15:42:40 +0100

    setting up github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/lms-project",
+  resolve: {
+    alias: {
+      "0": path.resolve(__dirname, "./src"),
+    },
+  },
+});
