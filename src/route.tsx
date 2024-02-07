42cae01209e0dee82f2323ffe0702bea09a07b66
deralph <jraphael441@gmail.com>
2024-02-07 12:25:44 +0100

    merged development branch

--- src/route.tsx
+++ src/route.tsx
@@ -4,6 +4,7 @@ import Auth from "./components/auth";
 import NotFound from "./pages/notFound";
 import AboutPage from "./pages/about";
 import Dashboard from "./components/student_dashboard";
+import AdminPage from "./pages/admin";
 
 const PageRoute = () => {
   return (
@@ -13,6 +14,7 @@ const PageRoute = () => {
       <Route path="/courses" element={<h1>Courses</h1>} />
       <Route path="/help" element={<h1>Need Help?</h1>} />
       <Route path="/login" element={<Auth />} />
+      <Route path="/admin" element={<AdminPage />} />
       <Route path="/student/dashboard" element={<Dashboard />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
